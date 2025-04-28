7e3a608aab20ce2eae5436be79bdda82195becea
eubrunaaraujo <bruna.ap85@gmail.com>
2025-04-28 18:11:11 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,4 @@
-/** @type {import('next').NextConfig} */\nconst nextConfig = {};\nmodule.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
